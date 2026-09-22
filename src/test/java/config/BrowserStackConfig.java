c643c21861b3b9f960a80f00f41e080f99895cec
kaliakbarova_ks721 <ainur.kaliakbarova@kaspi.kz>
2026-09-22 17:16:42 +0500

    Restore working BrowserStack config

--- src/test/java/config/BrowserStackConfig.java
+++ src/test/java/config/BrowserStackConfig.java
@@ -3,7 +3,10 @@ package config;
 import org.aeonbits.owner.Config;
 import org.aeonbits.owner.Config.Key;
 
-@Config.Sources("classpath:config.properties")
+@Config.Sources({
+        "system:properties",
+        "classpath:config.properties"
+})
 public interface BrowserStackConfig extends Config {
 
     @Key("browserstack.user")
